7e5e768f59fa205478d590458d567264f3855e22
thatkhay <johnkingsleyegeonu@gmail.com>
2023-11-03 22:06:38 +0100

    added navigation

--- my-app/App.js
+++ my-app/App.js
@@ -1,25 +1,50 @@
-  import React from 'react'
-  import { View, StyleSheet } from 'react-native'
+import React from 'react'
 import CurrentWeather from './src/screens/CurrentWeather'
 import UpcomingWeather from './src/screens/UpcomingWeather'
 import City from './src/screens/City'
-import {NavigationContainer} from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Feather } from '@expo/vector-icons'
 
-  const App = () => {
-    return (
-      <NavigationContainer>
-    <View style={styles.container}>
+const Tab = createBottomTabNavigator()
 
-<CurrentWeather />
-    </View>
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator 
+        screenOptions={{
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        }}
+      >
+        <Tab.Screen 
+          name="Current" 
+          component={CurrentWeather} 
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather name={"droplet"} size={25} color={focused ? "tomato" : "black"} />
+            ),
+          }}
+        />
+        <Tab.Screen name="Upcoming" component={UpcomingWeather} 
+        
+         options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather name={"clock"} size={25} color={focused ? "tomato" : "black"} />
+            ),
+          }}
+        />
+        <Tab.Screen name="City" component={City}
+        
+         options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather name={"home"} size={25} color={focused ? "tomato" : "black"} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
-    )
-  }
+  )
+}
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-  })
-  
-  export default App
+export default App
